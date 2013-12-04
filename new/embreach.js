ab47fd05a98fe574f802e0323aa1a52f5eb8d04f
Jari Aarniala <jari.aarniala@gmail.com>
2013-12-05 01:35:02 +0200

    Fancy styley

--- new/embreach.js
+++ new/embreach.js
@@ -3,8 +3,9 @@ SC.initialize({
   client_id: '34651ca078f5adf77323b80bc915596b'
 });
 SC.get('/playlists/' + playlist, function(playlist) {
+  $('#tracks').empty()
   $(playlist.tracks).each(function(index, track) {
     var week = index + 1
-    $('#tracks').append('<li><span class="week">Week ' + week + ':</span> ' + track.title + '</li>')
+    $('#tracks').append('<li class="track"><span class="week">Week ' + week + '</span> ' + track.title + '</li>')
   })
 })
