a710dc4e2e8b2c3ccbc414e688c0f59e29f91f0d
Jari Aarniala <jari.aarniala@gmail.com>
2013-12-05 01:19:22 +0200

    List tracks in page

--- new/embreach.js
+++ new/embreach.js
@@ -2,6 +2,9 @@ var playlist = '1305267'
 SC.initialize({
   client_id: '34651ca078f5adf77323b80bc915596b'
 });
-SC.get('/playlists/' + playlist + '.json', function(hep) {
-  console.log("Got", hep)
+SC.get('/playlists/' + playlist, function(playlist) {
+  $(playlist.tracks).each(function(index, track) {
+    var week = index + 1
+    $('#tracks').append('<li><span class="week">Week ' + week + ':</span> ' + track.title + '</li>')
+  })
 })
